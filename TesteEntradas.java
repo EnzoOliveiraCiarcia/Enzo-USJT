1fa13793420a4a410857b12411ce91c61c51d4dd
EnzoOliveiraCiarcia <enzooliveiraciarcia@gmail.com>
2026-03-16 19:47:57 -0300

    Add files via upload

---
+++ TesteEntradas.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TesteEntradas {
+    public static void main (String[] args) {        
+Scanner scanner = new Scanner (System.in); 
+int : nextInt();
+double : nextDouble();
+boolean : nextBoolean();
+scanner.close();
+
+    }
+}
