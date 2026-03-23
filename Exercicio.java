c95b570321ef1f08741a28cb659199977d9225f7
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-03-23 20:00:54 -0300

    Add files via upload

--- Exercicio.java
+++ Exercicio.java
@@ -1,16 +1,18 @@
-//Declarar 1 Variável inteira, uma double, uma String 
-//A inteira representa a idade de uma pessoa 
-//A String é o seu nome 
-//A double é a sua altura 
-//Atribui valores
-//Exibir a seguinte frase
-//Fulano,voce tem tantos anos e altura x 
+import java.util.Scanner;
 
 public class Exercicio {
     public static void main(String[] args) {
-        int idade = 18;
-        String nome ="Gabriel";
-        double altura = 1.83;
-        System.out.println ( nome + "" ,você tem " + idade + " e altura " + altura );
+        Scanner scanner = new Scanner(System.in);
+
+        double quilometros, milhas;
+
+        System.out.println("Digite a distância em quilômetros: ");
+        quilometros = scanner.nextDouble();
+
+        milhas = quilometros / 1.61;
+
+        System.out.println(quilometros + " km = " + milhas + " milhas");
+
+        scanner.close();
     }
 }
